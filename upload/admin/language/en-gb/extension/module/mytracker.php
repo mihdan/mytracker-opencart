2c5f289708a11d48323217fe16ab9c722d18d57a
Mikhail Kobzarev <mikhail@kobzarev.com>
2023-11-30 17:35:22 +0300

    Добавляет перевод на английский язык

---
+++ upload/admin/language/en-gb/extension/module/mytracker.php
@@ -0,0 +1,49 @@
+<?php
+$_['doc_title']  = 'MyTracker - VK Analytics';
+$_['heading_title']  = 'MyTracker - VK Analytics';
+
+$_['text_module_warning_heading']  = 'Warning';
+$_['text_module_warning_body']  = 'Uploading statistics into tracker database can take about one to four hours.';
+
+$_['text_extension']  = 'Extensions';
+$_['text_tab_general']  = 'General';
+$_['text_tab_general_description']  = '<b>MyTracker</b> is multi-platform analytics and attribution for mobile apps and websites. More details at <a href="https://tracker.my.com/" target="_blank">https://tracker.my.com/</a>.';
+$_['text_tab_additional']  = 'Additional';
+$_['text_tab_additional_description']  = 'Additional settings for the counter.';
+$_['text_tab_api']  = 'API';
+$_['text_tab_api_description']  = '<b>S2S</b> API allows you to send data to the MyTracker server. With this API, you can upload any events (for example, user registration and authorization) Uploaded events is added to the project data and displayed in MyTracker reports.';
+
+$_['text_enabled']  = 'Enabled';
+$_['text_disabled']  = 'Disabled';
+$_['text_edit']  = 'Extension settings';
+$_['text_success']  = 'MyTracker module settings are updated';
+
+$_['text_counter_id']  = 'Counter ID';
+$_['text_counter_id_help']  = 'Web Counter ID in your account MyTracker.';
+
+$_['text_entry_status'] = 'Status';
+$_['text_entry_status_help'] = 'Enable/Disable the module in the frontend.';
+
+$_['text_domain'] = 'Domain';
+$_['text_domain_help'] = 'To track website visits from regions where the VK services are available, use the RU domain. Select the COM domain if you need to track website visits from regions where the VK services are not available.';
+
+$_['text_tracking_user'] = 'Tracking user';
+$_['text_tracking_user_help'] = 'Allows you to track statistics not only on devices, but also on registered users of your site.';
+
+$_['text_amp_support'] = 'AMP Support';
+$_['text_amp_support_help'] = 'Enables analytics on AMP pages.';
+
+$_['text_app_id'] = 'APP ID';
+$_['text_app_id_help'] = 'Enter idApp web application from your MyTracker account.';
+
+$_['text_api_key'] = 'API key';
+$_['text_api_key_help'] = 'Enter S2S API key from your MyTracker account.';
+
+$_['text_tracking_login'] = 'Tracking login';
+$_['text_tracking_login_help'] = 'Tracking user authorization';
+
+$_['text_tracking_registration'] = 'Tracking registration';
+$_['text_tracking_registration_help'] = 'Tracking user registration.';
+
+$_['text_debug'] = 'Debugging';
+$_['text_debug_help'] = 'Debugging API queries.';
